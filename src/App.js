991a398c66b0891c52a78396be9fbf7b15f4f114
Tejas-Naik <RNTejas2005@gmail.com>
2023-03-24 06:46:46 -0700

    Creating a navigation bar and making that stick to all the pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { Route, Routes, Outlet } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <h1>Navigation</h1>
+      <Outlet />
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} >
-        <Route path="shop" element={<h1>Hello from shop page</h1>} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<h1>Shop Page</h1>} />
       </Route>
     </Routes>
   )
