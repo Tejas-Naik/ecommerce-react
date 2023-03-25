10964126b19b295ef34b0fddb717c28496ad2875
Tejas-Naik <RNTejas2005@gmail.com>
2023-03-25 06:47:25 -0700

    Creating users collection and checking if the user already exists

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,11 @@
-import { signInWithGooglePopUp } from "../../utils/firebase/firebase.utils";
+import { createUserDocumentFromAuth, db, signInWithGooglePopUp } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
     const logGoogleUser = async () => {
         const response = await signInWithGooglePopUp();
-        console.log(response);
+        createUserDocumentFromAuth(response.user);
     }
+
     return (
         <div>
             <h1>Sign In Page</h1>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,10 +1,13 @@
-import { getApp, initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
+// Authentication
 import {
     getAuth,
     GoogleAuthProvider,
     signInWithPopup,
     signInWithRedirect
 } from 'firebase/auth';
+// Firestore Database
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCmQgWC3ZYE0czTM1H8TtKUvQQQxP05-Jk",
@@ -18,6 +21,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
+// Authentication
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
     prompt: 'select_account'
@@ -25,3 +29,23 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopUp = () => signInWithPopup(auth, provider);
+
+// Firestore DB
+export const db = getFirestore(firebaseApp);
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    // const docRef = await addDoc(collection(db, "users"), {
+    //             displayName: response.user.displayName,
+    //             email: response.user.email,
+    //             createdAt: new Date(),
+    //         });
+
+    //         console.log("Document written with ID: ", docRef.id);
+}
