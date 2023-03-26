a5e58f597904ceec9afda627f2655ed2f10ee6c3
Tejas-Naik <RNTejas2005@gmail.com>
2023-03-25 17:09:07 -0700

    Styled Signup Form

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
+import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
     displayName: '',
@@ -50,8 +51,9 @@ const SignUpForm = () => {
     }
 
     return (
-        <div>
-            <h1>Sign Up with Email and Password</h1>
+        <div className="sign-up-container">
+            <h2>Don't have an account</h2>
+            <span>Sign Up with Email and Password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput label="Display Name" type="text" required onChange={handleChange} name="displayName" value={displayName} />
 
