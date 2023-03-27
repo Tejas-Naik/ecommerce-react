fd8d2550355f78e7f3f5364b58d5e9484f623bd3
Tejas-Naik <RNTejas2005@gmail.com>
2023-03-27 16:11:00 -0700

    displaying Total Items in cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import { CartContext } from '../../contexts/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
+import { Link } from 'react-router-dom';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
@@ -12,7 +13,7 @@ const CartDropdown = () => {
             <div className='cart-items'>
                 {cartItems.map((item) => <CartItem id={item.id} cartItem={item} />)}
             </div>
-            <Button>go Checkout</Button>
+            <Link to="/checkout"><Button>go to Checkout</Button></Link>
         </div>
     )
 }
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,10 +1,13 @@
 import './cart-item.styles.scss';
 
-const CartItem = ({ cartItem: { name, quantity, imageUrl } }) => {
+const CartItem = ({ cartItem: { name, quantity, price, imageUrl } }) => {
     return (
-        <div>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
+        <div className='cart-item-container'>
+            <img src={imageUrl} alt={name} />
+            <div className='item-details'>
+                <span className='name'>{name}</span>
+                <span className='price'>{quantity} x ${price}</span>
+            </div>
         </div>
     )
 }
