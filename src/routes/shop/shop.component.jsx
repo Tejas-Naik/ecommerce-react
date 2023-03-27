059b6d770da6c0ab20a2a9fa91cec856d92aa181
Tejas-Naik <RNTejas2005@gmail.com>
2023-03-27 04:30:42 -0700

    Adding the ShopData to the Contexts API

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,10 +1,13 @@
-import shopData from "../../shop-data.json";
+import { useContext } from "react";
+import { ProductsContext } from "../../contexts/products.context";
+
 const Shop = () => {
-    console.log(shopData); // name, id, price, imageUrl
+    const { products } = useContext(ProductsContext)
+    console.log(products); // name, id, price, imageUrl
     return (
         <>
             <h1>Shop page component</h1>
-            {shopData.map(({ id, name, price, imageUrl }) => (
+            {products.map(({ id, name, price, imageUrl }) => (
                 <div key={id}>
                     <h3>{name}</h3>
                     <p>{price}</p>
