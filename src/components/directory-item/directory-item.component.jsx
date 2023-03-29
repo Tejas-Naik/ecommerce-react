93d2c77b8082f28cdf875e5387a28a76c75d0732
Tejas-Naik <RNTejas2005@gmail.com>
2023-03-29 05:58:56 -0700

    Resolving the styles conflicting error

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,14 @@
+import './directory-item.styles.scss';
+const DirectoryItem = ({ category: { title, imageUrl } }) => {
+    return (
+        <div className="directory-item-container">
+            <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}></div>
+            <div className="body">
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    )
+};
+
+export default DirectoryItem;
