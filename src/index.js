43532a979fdb5a43863d8c31f7740d08de67032c
Tejas-Naik <RNTejas2005@gmail.com>
2023-03-31 17:19:03 -0700

    Redux will always cover the whole app

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { CartProvider } from './contexts/cart.context';
 import { CategoriesProvider } from './contexts/categories.context';
-// import { CategoriesProvider } from './contexts/categories.context';
-
 import { UserProvider } from './contexts/user.context';
 
 import './index.scss';
@@ -17,11 +15,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        {/* <CategoriesProvider> */}
-        <CartProvider>
-          <App />
-        </CartProvider>
-        {/* </CategoriesProvider> */}
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
