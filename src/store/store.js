baa38f1fc60f4eb0a7fb9b6b81fa7b705537be92
Tejas-Naik <RNTejas2005@gmail.com>
2023-04-02 08:59:35 -0700

    useSelect triggers re-rendering

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,24 @@
 import { compose, legacy_createStore as createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-
+// import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-    Boolean
-);
+// const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+//     Boolean
+// );
+
+const loggerMiddleWare = (store) => (next) => (action) => {
+    if (!action.type) return next(action);
+
+    console.log("type: ", action.type);
+    console.log("payload: ", action.payload);
+    console.log("currentState: ", store.getState());
+
+    next(action);
+
+    console.log("Next State: ", store.getState());
+}
+
+const middleWares = [loggerMiddleWare];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
