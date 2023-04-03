6f6e18e211e8dafe3e273e190556bb89ba63b62a
Tejas-Naik <RNTejas2005@gmail.com>
2023-04-03 06:15:04 -0700

    using redux thunk for async behavior of categories reducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,24 +2,17 @@ import { applyMiddleware, compose, legacy_createStore as createStore } from 'red
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-// import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
-// const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-//     Boolean
-// );
 
-const loggerMiddleWare = (store) => (next) => (action) => {
-    if (!action.type) return next(action);
+const middleWares = [process.env.NODE_ENV === 'development' && logger, thunk].filter(
+    Boolean
+);
+// const middleWares = [logger];
 
-    console.log("type: ", action.type);
-    console.log("payload: ", action.payload);
-    console.log("currentState: ", store.getState());
-
-    next(action);
-
-    console.log("Next State: ", store.getState());
-}
 
 const persistConfig = {
     key: 'root',
@@ -29,9 +22,8 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleWare];
-
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
